fe0c4407fb4532e70f6989244f5ac1c6f9d873c3
Heron Rodrigues <heronrs1@gmail.com>
2020-02-18 12:51:46 -0300

    acess index.html from server

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 
-
 const server = express();
 
+// Apresentar arquivos estáticos
+server.use(express.static('../frontend/public'))
+
+// Template engine
+nunjucks.configure('../frontend', {
+    express: server,
+    noCache: true
+})
+
+const donors = [
+    {
+        name: 'Heron Rodrigues',
+        blood: 'O+Zika'
+    }
+]
+
 server.get('/', (req, res) => {
-    res.send('Tamo-lhe junto, meu bom!!')
+    res.render('index.html', {donors});
 })
 
 server.listen(3000);
